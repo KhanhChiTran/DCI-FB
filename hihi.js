fdc0265ae6ab6df048b22b600a118c55992f74eb
ChiTran <Khanhchi.tran13051@gmail.com>
2021-01-25 23:26:27 +0100

    A very hard exercise but I solved it*clap**clap*

--- hihi.js
+++ hihi.js
@@ -64,3 +64,40 @@ function uncensor(str, vowels) {
 console.log(uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo"));
 console.log(uncensor("abcd", ""));
 console.log(uncensor("*PP*RC*S*", "UEAE"));
+
+//https://www.codewars.com/kata/555615a77ebc7c2c8a0000b8/train/javascript/600f21e2aa3655002ed207f2
+// The new "Avengers" movie has just been released! There are a lot of people at the cinema box office standing in a huge line. Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+
+// Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+// Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+
+// Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment. Otherwise return NO
+function tickets(peopleInLine) {
+  if (peopleInLine[0] != 25) return "NO";
+  if (peopleInLine[1] == 100 || peopleInLine[2] == 100) return "NO";
+  let holdCoin = {
+    25: 1,
+    50: 0,
+  };
+
+  for (let i = 1; i < peopleInLine.length; i++) {
+    if (peopleInLine[i] === 25) {
+      holdCoin["25"] += 1;
+    } else if (peopleInLine[i] == 50) {
+      holdCoin["50"] += 1;
+      holdCoin["25"] -= 1;
+      if (holdCoin["25"] < 0) return "NO";
+    } else {
+      if (holdCoin["50"] > 0) {
+        holdCoin["50"] -= 1;
+        holdCoin["25"] -= 1;
+        if (holdCoin["25"] < 0) return "NO";
+        continue;
+      }
+      holdCoin["25"] -= 3;
+      if (holdCoin["25"] < 0) return "NO";
+    }
+  }
+  return "YES";
+}
