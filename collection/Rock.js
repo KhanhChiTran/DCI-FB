b47217374bdf4947c582fb88ca48872f93154aab
ChiTran <Khanhchi.tran13051@gmail.com>
2021-01-28 21:11:41 +0100

    added random Productlists exc

--- collection/Rock.js
+++ collection/Rock.js
@@ -11,16 +11,60 @@
 // - paper beats rock
 // - scissors beats paper
 //Once we have the winner, we need to print to the console a message congratulating the winner, for example: `Computer wins. Rock beats paper. Congratulations, machine!`.
-const game = {
-  computer: getComputerChoice(),
-  player: "S",
-};
+// const game = {
+//   computer: getComputerChoice(),
+//   player: "S",
+// };
+// function getComputerChoice() {
+//   const choices = ["R", "P", "S"];
+//   const randomNumber = Math.floor(Math.random() * 3);
+//   return choices[randomNumber];
+// }
+// function play({ computer, player }) {
+//   let result = "";
+//   if (computer == player) {
+//     result = "It's a tie !";
+//   }
+//   if (computer == "R") {
+//     if (player == "S") {
+//       result = "Computer wins. Rock beats scissors. Congratulations, machine!";
+//     } else {
+//       result = "Player wins. Paper beats rock. Congratulations, player!";
+//     }
+//   }
+//   if (computer == "S") {
+//     if (player == "P") {
+//       result = "Computer wins. Scissors beats paper. Congratulations, machine!";
+//     } else {
+//       result = "Player wins. Rock beats scissors. Congratulations, player!";
+//     }
+//   }
+//   if (computer == "P") {
+//     if (player == "R") {
+//       result = "Computer wins. Paper beats rock. Congratulations, machine!";
+//     } else {
+//       result = "Player wins. Scissors beats paper. Congratulations, player!";
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(play(game));
+
 function getComputerChoice() {
   const choices = ["R", "P", "S"];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 }
-function play({ computer, player }) {
+function play() {
+  const game = {
+    computer: getComputerChoice(),
+    player: "P",
+  };
+  return getWinner(game);
+}
+
+function getWinner({ computer, player }) {
   let result = "";
   if (computer == player) {
     result = "It's a tie !";
@@ -49,4 +93,4 @@ function play({ computer, player }) {
   return result;
 }
 
-console.log(play(game));
+console.log(play());
