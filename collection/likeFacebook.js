f37e74735c3e8938fc84571f9ac9899c23d5dd6e
ChiTran <Khanhchi.tran13051@gmail.com>
2021-01-28 23:55:55 +0100

    practice switch statement

---
+++ collection/likeFacebook.js
@@ -0,0 +1,30 @@
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement a function likes :: [String] -> String, which must take in input array, containing the names of people who like an item. It must return the display text as shown in the examples:
+
+// likes [] -- must be "no one likes this"
+// likes ["Peter"] -- must be "Peter likes this"
+// likes ["Jacob", "Alex"] -- must be "Jacob and Alex like this"
+// likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
+// likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
+function likes(names) {
+  let alert = "";
+  switch (names.length) {
+    case 0:
+      alert = "no one likes this";
+      break;
+    case 1:
+      alert = `${names[0]} likes this`;
+      break;
+    case 2:
+      alert = `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      alert = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
+    default:
+      alert = `${names[0]}, ${names[1]} and ${names.length - 2} like this`;
+      break;
+  }
+  return alert;
+}
