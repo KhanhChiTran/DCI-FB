f497214beb8a8bd87147470f30ecc5c5bb4049fd
ChiTran <Khanhchi.tran13051@gmail.com>
2021-01-30 00:17:43 +0100

    Hard-working is the key!Come on!

--- collection/findInt.js
+++ collection/findInt.js
@@ -12,3 +12,14 @@ function solution(number) {
 //Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
 //Note: If the number is a multiple of both 3 and 5, only count it once. Also, if a number is negative, return 0(for languages that do have them
+
+//change passed string to CamelCase and convert all dash , underscore
+function toCamelCase(str) {
+  let newArr = str.split("");
+  for (let i = 0; i < newArr.length; i++) {
+    if (newArr[i] == "_" || newArr[i] == "-") {
+      newArr[i + 1] = newArr[i + 1].toUpperCase();
+    }
+  }
+  return newArr.filter((x) => x !== "-" && x !== "_").join("");
+}
