18aed225fb47be7930b7ab7930bbcacb1fb7b487
ChiTran <Khanhchi.tran13051@gmail.com>
2021-02-22 18:30:56 +0100

    Date() topic

---
+++ collection/ageOfMajority.js
@@ -0,0 +1,78 @@
+// # Age of majority​
+// Let's create a function (using closures) to create an age of majority validator.
+// The age of majority is the threshold of adulthood as recognized or declared in law. It is the moment when minors cease to be considered such and assume legal control over their persons, actions, and decisions, thus terminating the control and legal responsibilities of their parents or guardian over them.
+// Different countries have a different age of majority, so it should be possible to specify the counry (as a short code) we want to check for.
+// For example, with Germany, where the age of majority is 18:
+// ```js
+// const isAgeOfMajority = createAgeOfMajorityValidator('DE');
+// const timeLeft = legalTime - new Date();
+// let months = Math.floor(timeLeft / 60000 / 60 / 24 / 30);
+// console.log(months);
+// ```
+// and then we can use it like so:
+// - Myanmar (MM) - 15
+// - United Kingdom (GB) - 16
+// - Germany (DE) - 18
+// - Canada (CA) - 19
+// - Japan (JP) - 20
+function createAgeOfMajorityValidator(country) {
+  const countries = {
+    MM: 15,
+    GB: 16,
+    DE: 18,
+    CA: 19,
+    JP: 20,
+  };
+  return function (birthday) {
+    const birthYear = Number(birthday.slice(0, 4)) + countries[country];
+    const birthMonth = Number(birthday.slice(5, 7));
+    const birthDay = Number(birthday.slice(8, 10));
+    const legalTime = new Date(birthYear, birthMonth - 1, birthDay);
+    const x = new Date() - legalTime;
+    const y = x / (1000 * 60 * 60 * 24);
+    console.log(y);
+    return new Date() >= legalTime;
+  };
+}
+
+const isAgeOfMajority = createAgeOfMajorityValidator("DE");
+console.log(isAgeOfMajority("2019-11-11")); //false
+console.log(isAgeOfMajority("2003-04-23")); //false, few months to go
+console.log(isAgeOfMajority("2003-02-23")); //false, one more day to go
+console.log(isAgeOfMajority("2003-02-22")); //true, they turn 18 today!
+console.log(isAgeOfMajority("1920-11-11")); // 'true
+
+// # Born on a weekend
+// ​
+// Write a function that accepts a date of birth.
+// ​
+// It returns a string: `Born on a weekend` if the person was born on a weekend.
+// ​
+// If the birth date was not on a weekend, it should return: `Born on a week day: Monday`, for example.
+
+function isWeekend(year, month, date) {
+  const birth = new Date(year, month, date);
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const day = new Date(birth).getDay();
+  return day === 0 || day === 6
+    ? "Born on a weekend"
+    : `Born on a week day: ${weekDays[day]} `;
+}
+
+console.log(isWeekend(2021, 02, 21));
+console.log(isWeekend(2021, 02, 22));
+
+// # Friday the 13th
+// ​
+// Given the month and year as human readable numbers, return whether that month contains a Friday 13th.
+const assert = require("assert").strict;
+
+assert.deepEqual([[[1, 2, 3]], 4, 5], [[[1, 2, "3"]], 4, 5]);
--- collection/class-component.js
+++ collection/class-component.js
@@ -1,106 +1,106 @@
-class Person {
-  constructor(personName, personAge) {
-    this.name = personName;
-    this.age = personAge;
-  }
-  describe() {
-    return `${this.name}, ${this.age} years old`;
-  }
-}
-const student1 = new Person("John", 19);
-console.log(student1.describe());
+// class Person {
+//   constructor(personName, personAge) {
+//     this.name = personName;
+//     this.age = personAge;
+//   }
+//   describe() {
+//     return `${this.name}, ${this.age} years old`;
+//   }
+// }
+// const student1 = new Person("John", 19);
+// console.log(student1.describe());
 
-class Tv {
-  constructor(brand, channel = 1, volume = 50) {
-    this.brand = brand;
-    this.channel = channel;
-    this.volume = volume;
-  }
-  info() {
-    return `${this.brand} at channel ${this.channel}, volume ${this.volume}`;
-  }
-  increase() {
-    return this.volume < 100 ? this.volume++ : "No Signal";
-  }
-  decrease() {
-    return this.volume > 0 ? this.volume-- : "No Signal";
-  }
-  randomChannel() {
-    return (this.channel = Math.ceil(Math.random() * 50));
-  }
-  reset() {
-    this.channel = 1;
-    this.volume = 50;
-    return `${this.brand} at channel ${this.channel}, volume ${this.volume}`;
-  }
-}
+// class Tv {
+//   constructor(brand, channel = 1, volume = 50) {
+//     this.brand = brand;
+//     this.channel = channel;
+//     this.volume = volume;
+//   }
+//   info() {
+//     return `${this.brand} at channel ${this.channel}, volume ${this.volume}`;
+//   }
+//   increase() {
+//     return this.volume < 100 ? this.volume++ : "No Signal";
+//   }
+//   decrease() {
+//     return this.volume > 0 ? this.volume-- : "No Signal";
+//   }
+//   randomChannel() {
+//     return (this.channel = Math.ceil(Math.random() * 50));
+//   }
+//   reset() {
+//     this.channel = 1;
+//     this.volume = 50;
+//     return `${this.brand} at channel ${this.channel}, volume ${this.volume}`;
+//   }
+// }
 
-const tv1 = new Tv("Panasonic", 9, 100);
-console.log(tv1.info());
+// const tv1 = new Tv("Panasonic", 9, 100);
+// console.log(tv1.info());
 
 // console.log(tv1.increase());
-tv1.increase();
-tv1.increase();
-console.log(tv1.info());
+// tv1.increase();
+// tv1.increase();
+// console.log(tv1.info());
 // console.log(tv1.increase());
 // console.log(tv1.reset());
 // const tv2 = new Tv("Samsung");
 // console.log(tv2.info());
 
 //#3
-class Cylinder {
-  constructor(radius, height) {
-    this.radius = radius;
-    this.height = height;
-  }
-  getVolume() {
-    let volume = Math.PI * this.radius * this.radius * this.height;
-    return volume.toFixed(1);
-  }
-}
-const cylinder1 = new Cylinder(2.5, 10);
-console.log(cylinder1.getVolume());
-//#4
-class Employee {
-  constructor(
-    firstName = "",
-    lastName = "",
-    title = "Employee",
-    company = "Samsung",
-    phone = []
-  ) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.title = title;
-    this.company = company;
-    this.phone = phone;
-  }
-  createEmail() {
-    return `${this.firstName}.${this.lastName}@${this.company}.com`;
-  }
-  getFullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-  addPhoneNumber(...numbers) {
-    this.phone = this.phone.concat(numbers);
-    return this.phone;
-  }
-  getEmailSignature() {
-    return `
-     ${this.firstName} ${this.lastName}
-     ${this.title} at ${this.company}
-     -----------------------
-     E : ${this.createEmail()}
-     T : ${this.phone}
-    `;
-  }
-}
+// class Cylinder {
+//   constructor(radius, height) {
+//     this.radius = radius;
+//     this.height = height;
+//   }
+//   getVolume() {
+//     let volume = Math.PI * this.radius * this.radius * this.height;
+//     return volume.toFixed(1);
+//   }
+// }
+// const cylinder1 = new Cylinder(2.5, 10);
+// console.log(cylinder1.getVolume());
+// //#4
+// class Employee {
+//   constructor(
+//     firstName = "",
+//     lastName = "",
+//     title = "Employee",
+//     company = "Samsung",
+//     phone = []
+//   ) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.title = title;
+//     this.company = company;
+//     this.phone = phone;
+//   }
+//   createEmail() {
+//     return `${this.firstName}.${this.lastName}@${this.company}.com`;
+//   }
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   }
+//   addPhoneNumber(...numbers) {
+//     this.phone = this.phone.concat(numbers);
+//     return this.phone;
+//   }
+//   getEmailSignature() {
+//     return `
+//      ${this.firstName} ${this.lastName}
+//      ${this.title} at ${this.company}
+//      -----------------------
+//      E : ${this.createEmail()}
+//      T : ${this.phone}
+//     `;
+//   }
+// }
 
-const employee1 = new Employee("Jessica", "Smith");
-employee1.addPhoneNumber("03045678", "0912345678");
-employee1.addPhoneNumber("123567");
+// const employee1 = new Employee("Jessica", "Smith");
+// employee1.addPhoneNumber("03045678", "0912345678");
+// employee1.addPhoneNumber("123567");
 
-console.log(employee1.getEmailSignature());
+// console.log(employee1.getEmailSignature());
 //# 5. Tick Tock
 // const template = "h:m:s";
 // let date = new Date();
@@ -141,3 +141,21 @@ class Template {
 
 const time = new Template();
 console.log(time.getTime());
+
+class Time {
+  constructor(template = "h:m:s") {
+    this.template = template;
+  }
+  getTime() {
+    const date = new Date();
+    const hours = date.getHours().toString().padStart(2, "0");
+    const mins = date.getMinutes().toString().padStart(2, "0");
+    const secs = date.getSeconds().toString().padStart(2, "0");
+    return this.template
+      .replace("h", hours)
+      .replace("m", mins)
+      .replace("s", secs);
+  }
+}
+const time1 = new Time();
+const time2 = new Time("h:m");
